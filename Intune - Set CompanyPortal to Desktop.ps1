320e4a32041346ef96619b85a6cf8321b1602f13
cranky_sysadmin <kaleb_palmby@yahoo.com>
2023-07-05 16:13:31 -0400

    Intune - Set Company Portal to Desktop
    Sets the Company Portal to the Desktop

---
+++ Intune - Set CompanyPortal to Desktop.ps1
@@ -0,0 +1,8 @@
+$TargetFile =  "C:\Windows\explorer.exe"
+$ShortcutFile = "C:\users\public\Desktop"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.Arguments="shell:AppsFolder\Microsoft.CompanyPortal_8wekyb3d8bbwe!App"
+$Shortcut.IconLocation = "https://deviceadvice.io/wp-content/uploads/2020/08/CompanyPortalApp.ico"
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
